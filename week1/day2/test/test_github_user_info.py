c1d3a916e756777b3bc9077c5d4e46c8eeef0623
Maksim28108 <bilukha.maksim@student.ehu.lt>
2026-09-06 22:26:38 +0300

    w1d3

--- week1/day2/test/test_github_user_info.py
+++ week1/day2/test/test_github_user_info.py
@@ -2,4 +2,7 @@ from qaptlrn.week1.day2.test.github_user_info import get_github_user_info
 
 def test_answer():
     data = get_github_user_info("octocat")
-    assert data.status_code == 200
+    assert 'login' in data
+    assert 'id' in data
+    assert data['login'] == "octocat"
+    assert isinstance(data['public_repos'], int)
